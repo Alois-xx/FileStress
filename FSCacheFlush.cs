7f13fd2a77c882708bd50f9aad3f58ff4ca64125
Alois-xx <akraus1@gmx.de>
2021-05-20 17:22:39 +0200

    Added support to flush file system contents for specific folders or files. That allows one to perform repeated tests which really read from the hard disk and not from the file system cache. This will not flush any disk controller caches like RAID controllers. But at least the Windows Caches are flushed by this. There are always more cache indirections in between if you get unbelievable high disk throughput rates you need to check how you can disable the RAID controller cache if you need reliable numbers.

---
+++ FSCacheFlush.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStress
+{
+    class FSCacheFlush
+    {
+        List<string> myFiles = new List<string>();
+
+        public FSCacheFlush(string fileOrDirectory) : this(fileOrDirectory, SearchOption.TopDirectoryOnly)
+        { }
+
+        public FSCacheFlush(string fileOrDirectory, SearchOption option)
+        {
+            if (Directory.Exists(fileOrDirectory))
+            {
+                myFiles.AddRange(Directory.GetFiles(fileOrDirectory, "*.*", option));
+            }
+            else if (File.Exists(fileOrDirectory))
+            {
+                myFiles.Add(fileOrDirectory);
+            }
+            else
+            {
+                throw new FileNotFoundException($"File/Directory {fileOrDirectory} does not exist.");
+            }
+
+        }
+
+        public void Flush()
+        {
+            foreach(var file in myFiles)
+            {
+                FlushFileSystemCacheForFile(file);
+            }
+        }
+
+        void FlushFileSystemCacheForFile(string fileName)
+        {
+            const FileOptions FileFlagNoBuffering = (FileOptions)0x20000000;
+
+            using (var file = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 4096, FileFlagNoBuffering))
+            {
+                // Just open close the file to flush file system cache with FileFlagNoBuffering
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,12 +10,12 @@ namespace FileStress
     {
         const int DefaultSizeSizeMB = 10;
 
-        static readonly string HelpStr = 
+        static readonly string HelpStr =
            $"FileStress v{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}" + Environment.NewLine +
-            " FileStress [-throughput dd [-keepFiles] [-nthreads nn] [-filesizemb dd] [-norandom]] [-map [dd] [-nounmap] [-filesizemb dd] [-keepFiles]] [-filecreate [-keepFiles]] [-touchgb dd] [-committgb dd] [c:] [d:] [e:] [f:]..." + Environment.NewLine +
+            " FileStress [-throughput dd [-keepFiles] [-nthreads nn] [-filesizemb dd] [-norandom]] [-map [dd] [-nounmap] [-filesizemb dd] [-keepFiles]] [-flush fileorDir [-recursive]] [-filecreate [-keepFiles]] [-touchgb dd] [-committgb dd] [c:] [d:] [e:] [f:]..." + Environment.NewLine +
            $"  -throughput dd    Test drive thoughput for dd minutes by writing {DefaultSizeSizeMB} MB files from two threads to \\{FolderName}." + Environment.NewLine +
-            "              dd    dd is the runtime of the test in minutes. If the disk becomes full during the test it will delete the generated files and continue until the configured runtime is reached." + Environment.NewLine + 
-            "                    You can also use fractions of minutes to test a short run. E.g. -throughput 0.1" + Environment.NewLine + 
+            "              dd    dd is the runtime of the test in minutes. If the disk becomes full during the test it will delete the generated files and continue until the configured runtime is reached." + Environment.NewLine +
+            "                    You can also use fractions of minutes to test a short run. E.g. -throughput 0.1" + Environment.NewLine +
             "     -nthreads n    Number of concurrent writes. Default is 2" + Environment.NewLine +
            $"     -filesizemb n  Size of file to be written. Default is {DefaultSizeSizeMB}" + Environment.NewLine +
             "     -norandom      By default random data is written to the files. Otherwise a simple pattern with A is written to the files" + Environment.NewLine +
@@ -24,18 +24,24 @@ namespace FileStress
            $"     -filesizemb n  Size of file to be written. Default is {DefaultSizeSizeMB}" + Environment.NewLine +
             "     -nounmap       Do not unmap the data until it is written to keep the data in the current process working set" + Environment.NewLine +
             "     -keepFiles     Do not deleted created temporary files on exit" + Environment.NewLine +
+            "  -flush fileOrDir  Flush file system cache for a file or all files in a folder" + Environment.NewLine +
+            "      -recursive    If used then all files below that directory file also be flushed" + Environment.NewLine +
            $"  -filecreate       File Creation Test which will create 20K files in the folder \\{FolderName} on the target drive" + Environment.NewLine +
             "     -keepFiles     Do not deleted created temporary files on exit. This way you can test e.g. NTFS Directory traversal performance with hundreds of thousands of files with subsequent calls." + Environment.NewLine +
             " -touchgb   dd      Commit and touch dd GB of memory before other tests start" + Environment.NewLine +
             " -committgb dd      Commit but do not touch memory before other tests start" + Environment.NewLine +
             " -waitforenter      Wait for an enter press before exiting. That way you can create several GB sized allocations which can be released later interactivly from the shell." + Environment.NewLine +
-            "Examples" + Environment.NewLine + 
+            "Examples" + Environment.NewLine +
             "Allocate 10 GB of memory to put system under memory stress" + Environment.NewLine +
-            "  FileStress -touchgb 10 -waitforenter" + Environment.NewLine + 
+            "  FileStress -touchgb 10 -waitforenter" + Environment.NewLine +
             "Test Disk Throughput with 2 threads writing random data in 10MB sized files for 30 minutes" + Environment.NewLine +
-            "  FileStress -throughput 30 C:" + Environment.NewLine + 
-            "Create 30 Files/s in page file backed memory maps which are written by a different thread to the file system as normal files."+ Environment.NewLine +
-            " FileStress.exe -map c:";
+            "  FileStress -throughput 30 C:" + Environment.NewLine +
+            "Create 30 Files/s in page file backed memory maps which are written by a different thread to the file system as normal files." + Environment.NewLine +
+            " FileStress.exe -map c:" + Environment.NewLine +
+            "Flush file system cache of all files in that folder and subfolders" + Environment.NewLine +
+            " FileStress -flush C:\\data -recursive" + Environment.NewLine;
+
+
 
         enum Mode
         {
@@ -44,6 +50,7 @@ namespace FileStress
             FileCreate,
             Throughput,
             Allocate,
+            Flush,
         }
 
         const string FolderName = "TempFilePerformanceTest";
@@ -63,6 +70,10 @@ namespace FileStress
             float nMinuteRuntime = 1;
             bool deleteTempFilesOnExit = true;
             string outputFolder = null;
+
+            string flushFileFolder = null;
+            bool recursive = false;
+
 
             void CleanFilesOnExit()
             {
@@ -104,10 +115,10 @@ namespace FileStress
                 {
                     case "-touchgb":
                         touchMemory = true;
-                        GB = NextNumberOrDefault(qargs, 0);
+                        GB = NextArgumentOrDefault(qargs, 0);
                         break;
                     case "-committgb":
-                        GB = NextNumberOrDefault(qargs, 0);
+                        GB = NextArgumentOrDefault(qargs, 0);
                         break;
                     case "-waitforenter":
                         waitForExit = true;
@@ -120,16 +131,23 @@ namespace FileStress
                         break;
                     case "-throughput":
                         mode = Mode.Throughput;
-                        nMinuteRuntime = NextNumberOrDefault(qargs, 5.0f);
+                        nMinuteRuntime = NextArgumentOrDefault(qargs, 5.0f);
                         break;
                     case "-nthreads":
-                        nThreads = NextNumberOrDefault(qargs, 2);
+                        nThreads = NextArgumentOrDefault(qargs, 2);
                         break;
                     case "-filesizemb":
-                        fileSizeMB = NextNumberOrDefault(qargs, DefaultSizeSizeMB);
+                        fileSizeMB = NextArgumentOrDefault(qargs, DefaultSizeSizeMB);
                         break;
                     case "-norandom":
                         randomData = false;
+                        break;
+                    case "-flush":
+                        mode = Mode.Flush;
+                        flushFileFolder = NextArgumentOrDefault(qargs, (string) null);
+                        break;
+                    case "-recursive":
+                        recursive = true;
                         break;
                     case "c:":
                     case "d:":
@@ -148,7 +166,7 @@ namespace FileStress
                         mode = Mode.Mapping;
                         break;
                     default:
-                        var parsed = ParseNumber(currentArg, 0.0f);
+                        var parsed = ReadValue(currentArg, 0.0f);
                         if( parsed.Value)
                         {
                             switch (mode)
@@ -200,14 +218,21 @@ namespace FileStress
                         MappedWriterBase writer = new MappedWriter((int)fileSizeMB, outputFolder, noUnmap);
                         writer.StartGeneration(nFilesPerSecond);
                         writer.Write();
+                        CleanFilesOnExit();
                         break;
                     case Mode.FileCreate:
                         var filePerf = new FileCreationPerformance();
                         filePerf.TestFileCreationPerformance(outputFolder, 20_000, 0, false);
+                        CleanFilesOnExit();
                         break;
                     case Mode.Throughput:
                         var test = new ThroughputTest(outputFolder, nMinuteRuntime, fileSizeMB, nThreads, randomData, CleanFilesOnExit);
                         test.Run();
+                        CleanFilesOnExit();
+                        break;
+                    case Mode.Flush:
+                        FSCacheFlush flusher = new FSCacheFlush(flushFileFolder, recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+                        flusher.Flush();
                         break;
                     default:
                         Help();
@@ -225,8 +250,7 @@ namespace FileStress
                 Help();
                 Console.WriteLine($"Got Exception: {ex}");
             }
-
-            CleanFilesOnExit();
+            
         }
 
         private static void Help()
@@ -234,13 +258,13 @@ namespace FileStress
             Console.WriteLine(HelpStr);
         }
 
-        static T NextNumberOrDefault<T>(Queue<string> args, T defaultNr)
+        static T NextArgumentOrDefault<T>(Queue<string> args, T defaultValue)
         {
-            T lret = defaultNr;
+            T lret = defaultValue;
             if (args.Count > 0)
             {
                 string tryArg = args.Peek();
-                var parsed = ParseNumber(tryArg, defaultNr);
+                var parsed = ReadValue(tryArg, defaultValue);
                 lret = parsed.Key;
                 if (parsed.Value == true)
                 {
@@ -250,7 +274,7 @@ namespace FileStress
             return lret;
         }
 
-        static KeyValuePair<T, bool> ParseNumber<T>(string str, T defaultNr)
+        static KeyValuePair<T, bool> ReadValue<T>(string str, T defaultNr)
         {
             T lret;
             bool success = true;
@@ -266,7 +290,7 @@ namespace FileStress
                     lret = defaultNr;
                 }
             }
-            else
+            else if( typeof(T) == typeof(int) )
             {
                 if (int.TryParse(str, out int parsedNr))
                 {
@@ -277,6 +301,14 @@ namespace FileStress
                     success = false;
                     lret = defaultNr;
                 }
+            }
+            else if( typeof(T) == typeof(string) )
+            {
+                lret = (T)(object)str;
+            }
+            else
+            {
+                throw new InvalidCastException($"Type {typeof(T).FullName} is not implemented.");
             }
 
             return new KeyValuePair<T, bool>(lret, success);
